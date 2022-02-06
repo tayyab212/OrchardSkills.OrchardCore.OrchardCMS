ba1006163e9029c888d04d52ce138516767a7205
tayyab212 <tthunjra@gmail.com>
2022-02-06 13:55:09 +0500

    This is a combination of 2 commits.

---
+++ OrchardSkills.OrchardCore.SaaS/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "SaaS",
+    Author = "Orchard Skills",
+    Website = "http://OrchardSkills.com",
+    Version = "1.0.0-rc2",
+    Description = "A SaaS Multi-Tenant website."
+)]
